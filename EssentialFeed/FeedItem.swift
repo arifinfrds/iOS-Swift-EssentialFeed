64ba8fde90978847b7feb1858f705253c9508bfc
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2019-10-06 07:35:05 +0700

    Create Feed Item Model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Arifin Firdaus on 10/6/19.
+//  Copyright © 2019 arifinfrds. All rights reserved.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let url: URL
+}
