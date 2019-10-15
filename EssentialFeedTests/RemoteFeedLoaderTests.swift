a269c661077e3ea24348a28ece71404d42fcb8f9
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2019-10-15 21:31:59 +0700

    Make the `HTTPClient` a protocol

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -10,26 +10,28 @@ import XCTest
 @testable import EssentialFeed
 
 class RemoteFeedLoader {
+    
+    private let client: HTTPClient
+    
+    init(client: HTTPClient) {
+        self.client = client
+    }
  
     func load() {
-        HTTPClient.shared.get(from: URL(string: "https://a-url.com")!)
+        client.get(from: URL(string: "https://a-url.com")!)
     }
     
 }
 
-class HTTPClient {
-    
-    static var shared = HTTPClient()
-        
-    func get(from url: URL) {} 
-    
+protocol HTTPClient {
+    func get(from url: URL)
 }
 
 class HTTPClientSpy: HTTPClient {
     
     var requestedURL: URL?
     
-    override func get(from url: URL) {
+    func get(from url: URL) {
         requestedURL = url
     }
     
@@ -40,16 +42,14 @@ class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doestNotRequestDataFromURL() {
         let client = HTTPClientSpy()
-        HTTPClient.shared = client
-        let _ = RemoteFeedLoader()
+        let _ = RemoteFeedLoader(client: client)
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_requestDataFromURL() {
         let client = HTTPClientSpy()
-        HTTPClient.shared = client
-        let sut = RemoteFeedLoader()
+        let sut = RemoteFeedLoader(client: client)
         
         sut.load()
         
