be30cd9584d806d665590ee7396c4dff216aee3a
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2021-02-06 20:17:57 +0700

    Using ephemeral URLSessionConfiguration since no need to have caching by default for testing

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -39,7 +39,8 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
         // given
         let testServerURL = "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5c52cdd0b8a045df091d2fff/1548930512083/feed-case-study-test-api-feed.json"
         let url = URL(string: testServerURL)!
-        let client = URLSessionHTTPClient()
+        let session = URLSession(configuration: .ephemeral)
+        let client = URLSessionHTTPClient(session: session)
         let loader = RemoteFeedLoader(url: url, client: client)
         trackForMemoryLeaks(for: client)
         trackForMemoryLeaks(for: loader)
