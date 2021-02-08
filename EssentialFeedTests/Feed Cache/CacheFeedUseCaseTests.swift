007b44029cf90cc0ca02d32dc69482560c91b365
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2021-02-08 09:22:44 +0700

    does not delete cache upon creation

---
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -0,0 +1,32 @@
+//
+//  CacheFeedUseCaseTests.swift
+//  EssentialFeedTests
+//
+//  Created by Arifin Firdaus on 08/02/21.
+//  Copyright © 2021 arifinfrds. All rights reserved.
+//
+
+import XCTest
+
+class LocalFeedLoader {
+    private let store: FeedStore
+    
+    init(store: FeedStore) {
+        self.store = store
+    }
+}
+
+class FeedStore {
+    var deleteCacheCallCount = 0
+}
+
+class CacheFeedUseCaseTests: XCTestCase {
+
+    func test_init_doesNotDeleteCacheUponCreation() {
+        let store = FeedStore()
+        _ = LocalFeedLoader(store: store)
+    
+        XCTAssertEqual(store.deleteCacheCallCount, 0)
+    }
+    
+}
