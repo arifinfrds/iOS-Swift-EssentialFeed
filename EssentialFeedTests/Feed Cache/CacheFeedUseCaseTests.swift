5c9d75829f7cdc4cd99ee7a8a5169bbc28f0e8a8
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2021-02-08 09:41:48 +0700

    save command does not request insertion when complete with error

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -19,14 +19,18 @@ class LocalFeedLoader {
     func save(_ items: [FeedItem]) {
         store.deleteCachedFeed()
     }
+    
 }
 
 class FeedStore {
     var deleteCacheFeedCallCount = 0
+    var insertCallCount = 0
     
     func deleteCachedFeed() {
         deleteCacheFeedCallCount += 1
     }
+    
+    func completeDeletion(with error: Error, at index: Int = 0) { }
 }
 
 class CacheFeedUseCaseTests: XCTestCase {
@@ -46,6 +50,17 @@ class CacheFeedUseCaseTests: XCTestCase {
         XCTAssertEqual(store.deleteCacheFeedCallCount, 1)
     }
     
+    func test_save_doesNotRequestInsertionOnDeletionError() {
+        let (sut, store) = makeSUT()
+        let items = [uniqueItem(), uniqueItem()]
+        let deletionError = anyNSError()
+        
+        sut.save(items)
+        store.completeDeletion(with: deletionError)
+        
+        XCTAssertEqual(store.insertCallCount, 0)
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStore) {
@@ -62,6 +77,10 @@ class CacheFeedUseCaseTests: XCTestCase {
     
     private func anyURL() -> URL {
         return URL(string: "https://any-url.com")!
+    }
+    
+    private func anyNSError() -> NSError {
+        return NSError(domain: "Any error", code: 0)
     }
     
 }
