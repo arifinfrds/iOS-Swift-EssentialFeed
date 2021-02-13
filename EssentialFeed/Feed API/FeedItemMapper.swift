6225411de7c118297cdfdf37399772c1584e7041
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2021-02-13 10:33:38 +0700

    Move `RemoteFeedItem` and `LocalFeedItem` to its own file

--- EssentialFeed/Feed API/FeedItemMapper.swift
+++ EssentialFeed/Feed API/FeedItemMapper.swift
@@ -8,13 +8,6 @@
 
 import Foundation
 
-internal struct RemoteFeedItem: Decodable {
-    internal let id: UUID
-    internal let description: String?
-    internal let location: String?
-    internal let image: URL
-}
-
 final class FeedItemsMapper {
     
     private struct Root: Decodable {
--- EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -15,18 +15,3 @@ public protocol FeedStore {
     func deleteCachedFeed(completion: @escaping DeletionCompletion)
     func insert(_ items: [LocalFeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
 }
-
-
-public struct LocalFeedItem: Equatable {
-    public let id: UUID
-    public let description: String?
-    public let location: String?
-    public let url: URL
-    
-    public init(id: UUID, description: String?, location: String?, url: URL) {
-        self.id = id
-        self.description = description
-        self.location = location
-        self.url = url
-    }
-}
