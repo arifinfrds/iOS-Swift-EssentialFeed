39230de5ee75d7261deb480df071f291f3f85847
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2021-03-02 11:32:49 +0700

    load closure return a result type instead of optional error

--- EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -31,8 +31,13 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         let retrievalError = anyNSError()
         
         var receivedError: Error?
-        sut.load { error in
-            receivedError = error
+        sut.load { result in
+            switch result {
+            case let .failure(error):
+                receivedError = error
+            default:
+                XCTFail("Expected failure with error, got success instead.")
+            }
             exp.fulfill()
         }
         store.completeRetrieval(with: retrievalError)
