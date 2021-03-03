52398d4a9a2f86cccb7b05fab806c7764447ec93
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2021-03-03 14:19:18 +0700

    load command does not delivers result after SUT instance has been deallocated (to prevent unexpected received result on the client side)

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -41,7 +41,9 @@ public final class LocalFeedLoader {
     }
     
     public func load(completion: @escaping (LoadResult) -> Void) {
-        store.retrieve { [unowned self] result in
+        store.retrieve { [weak self] result in
+            guard let self = self else { return }
+            
             switch result {
             case .failure(let error):
                 self.store.deleteCachedFeed { _ in }
