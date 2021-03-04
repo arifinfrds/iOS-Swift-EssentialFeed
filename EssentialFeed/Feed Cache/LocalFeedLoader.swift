288498ff6983e4a6758b55e4fd6572cf3e0c6f9e
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2021-03-04 09:13:37 +0700

    add whitespace for easier readability

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -48,11 +48,14 @@ public final class LocalFeedLoader {
             case .failure(let error):
                 self.store.deleteCachedFeed { _ in }
                 completion(.failure(error))
+                
             case let .found(feed, timestamp) where self.validate(timestamp):
                 completion(.success(feed.toModels()))
+                
             case .found:
                 self.store.deleteCachedFeed { _ in }
                 completion(.success([]))
+                
             case .empty:
                 completion(.success([]))
             }
