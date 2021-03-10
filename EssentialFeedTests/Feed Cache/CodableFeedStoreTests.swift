f9f838a01ff6f2aad82a8595eeea32888ac09d86
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2021-03-10 09:35:16 +0700

    using make SUT factory method helper to create SUT instance

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -75,7 +75,7 @@ class CodableFeedStoreTests: XCTestCase {
     }
     
     func test_retrieve_deliversEmptyOnEmptyCache() {
-        let sut = CodableFeedStore()
+        let sut = makeSUT()
         let exp = expectation(description: "Wait for completion")
         
         sut.retrieve { result in
@@ -91,7 +91,7 @@ class CodableFeedStoreTests: XCTestCase {
     }
     
     func test_retrieveTwice_hasNoSideEffectsOnEmptyCache() {
-        let sut = CodableFeedStore()
+        let sut = makeSUT()
         let exp = expectation(description: "Wait for completion")
         
         sut.retrieve { firstResult in
@@ -110,7 +110,7 @@ class CodableFeedStoreTests: XCTestCase {
     }
     
     func test_retrieveAfterInsertToEmptyCache_deliversInsertedValues() {
-        let sut = CodableFeedStore()
+        let sut = makeSUT()
         let feed = uniqueImageFeed()
         let timestamp = Date()
         let exp = expectation(description: "Wait for completion")
@@ -129,6 +129,12 @@ class CodableFeedStoreTests: XCTestCase {
             }
         }
         wait(for: [exp], timeout: 1.0)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT() -> CodableFeedStore {
+        return CodableFeedStore()
     }
     
 }
