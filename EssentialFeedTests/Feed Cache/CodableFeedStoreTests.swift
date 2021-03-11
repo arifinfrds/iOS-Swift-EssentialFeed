bdb0570243aadb19b3fdd19d21fb886521972a0b
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2021-03-11 16:05:49 +0700

    Insert delivers error on insertion error

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -58,11 +58,15 @@ class CodableFeedStore {
     }
     
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
-        let encoder = JSONEncoder()
-        let cache = Cache(feed: feed.map(CodableFeedImage.init), timestamp: timestamp)
-        let encoded = try! encoder.encode(cache)
-        try! encoded.write(to: storeURL)
-        completion(nil)
+        do {
+            let encoder = JSONEncoder()
+            let cache = Cache(feed: feed.map(CodableFeedImage.init), timestamp: timestamp)
+            let encoded = try encoder.encode(cache)
+            try encoded.write(to: storeURL)
+            completion(nil)
+        } catch {
+            completion(error)
+        }
     }
 }
 
@@ -142,6 +146,17 @@ class CodableFeedStoreTests: XCTestCase {
         XCTAssertNil(secondInsertionError, "Expect to insert without error, got \(String(describing: secondInsertionError)) instead.")
         
         expect(sut, toRetrieve: .found(feed: latestFeed, timestamp: latestTimestamp))
+    }
+    
+    func test_insert_deliversErrorOnInsertionError() {
+        let invalidStoreURL = URL(string: "invalid://store-url")!
+        let sut = makeSUT(storeURL: invalidStoreURL)
+        let feed = uniqueImageFeed().local
+        let timestamp = Date()
+        
+        let insertionError = insert((feed, timestamp), to: sut)
+        
+        XCTAssertNotNil(insertionError)
     }
     
     // MARK: - Helpers
--- EssentialFeedTests/Feed Cache/FeedStore implementation Inbox.md
+++ EssentialFeedTests/Feed Cache/FeedStore implementation Inbox.md
@@ -10,7 +10,7 @@ Retrieve
 Insert
     - ✅ to empty cache works
     - ✅ to non-empty cache overrides previous value
-    - Error (if possible e.g., no write permission)
+    - ✅ Error (if possible e.g., no write permission)
 
 Delete
     - empty cache does nothing (cache stays empty and does not fail)
