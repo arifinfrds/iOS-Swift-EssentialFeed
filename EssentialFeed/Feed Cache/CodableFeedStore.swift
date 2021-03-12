7a2e6b8113513a2f189e9641078ccde269bcdb79
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2021-03-12 13:51:28 +0700

    allow non side effect CodableFeedStore implementation to run concurrently and make another side effect CodableFeedStore implementation run serially using flags `barrier` and default shared queues' attribute `concurrent` (to make retrieve API fast since it is concurrent and other API that has side effect waiting with `barrier` flag behavior

--- EssentialFeed/Feed Cache/CodableFeedStore.swift
+++ EssentialFeed/Feed Cache/CodableFeedStore.swift
@@ -36,13 +36,13 @@ public final class CodableFeedStore: FeedStore {
         }
     }
     
+    private let queue = DispatchQueue(label: "\(type(of: CodableFeedStore.self))Queue", qos: .userInitiated, attributes: .concurrent)
+    
     private let storeURL: URL
     
     public init(storeURL: URL) {
         self.storeURL = storeURL
     }
-    
-    private let queue = DispatchQueue(label: "\(type(of: CodableFeedStore.self))Queue", qos: .userInitiated)
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
         let storeURL = self.storeURL
@@ -63,7 +63,7 @@ public final class CodableFeedStore: FeedStore {
     
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
         let storeURL = self.storeURL
-        queue.async {
+        queue.async(flags: .barrier) {
             do {
                 let encoder = JSONEncoder()
                 let cache = Cache(feed: feed.map(CodableFeedImage.init), timestamp: timestamp)
@@ -78,7 +78,7 @@ public final class CodableFeedStore: FeedStore {
     
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         let storeURL = self.storeURL
-        queue.async {
+        queue.async(flags: .barrier) {
             guard FileManager.default.fileExists(atPath: storeURL.path) else {
                 completion(nil)
                 return
