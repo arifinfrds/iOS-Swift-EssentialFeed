a064a58534ba7c4fabdfdd734c76b1a83ddff6fc
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2021-03-21 11:17:40 +0700

    add `ManagedCache` and `ManagedFeedImage` core data model

--- EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -9,6 +9,8 @@
 import XCTest
 import EssentialFeed
 
+import CoreData
+
 final class CoreDataFeedStore: FeedStore {
     
     func retrieve(completion: @escaping RetrievalCompletion) {
@@ -22,6 +24,19 @@ final class CoreDataFeedStore: FeedStore {
     func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         
     }
+}
+
+private class ManagedCache: NSManagedObject {
+    @NSManaged var timestamp: Date
+    @NSManaged var feed: NSOrderedSet
+}
+
+private class ManagedFeedImage: NSManagedObject {
+    @NSManaged var id: UUID
+    @NSManaged var imageDescription: String?
+    @NSManaged var location: String?
+    @NSManaged var url: URL
+    @NSManaged var cache: ManagedCache
 }
 
 class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
@@ -59,7 +74,7 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     }
     
     func test_delete_deliversNoErrorOnEmptyCache() {
-    
+        
     }
     
     func test_delete_hasNoSideEffectsOnEmptyCache() {
@@ -84,5 +99,5 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
         let sut = CoreDataFeedStore()
         return sut
     }
-        
+    
 }
