d15d5328d677bc91b5bed88404e6b7bf3c906c01
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2021-03-22 13:33:39 +0700

    `CoreDataFeedStore.delete()` delivers no error on empty cache

--- EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -50,7 +50,7 @@ public final class CoreDataFeedStore: FeedStore {
     }
     
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        
+        completion(nil)
     }
 }
 
