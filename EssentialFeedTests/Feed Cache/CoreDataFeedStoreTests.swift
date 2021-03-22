f795a02846f383dec32029587e1dcd4aa8877398
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2021-03-22 08:07:14 +0700

    Make `StoreURL` explicit so that we can inject the store specific url to control the url between production and tests store URL

--- EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -67,7 +67,8 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> CoreDataFeedStore {
         let bundle = Bundle(for: CoreDataFeedStore.self)
-        let sut = try! CoreDataFeedStore(bundle: bundle)
+        let storeURL = URL(fileURLWithPath: "/dev/null")
+        let sut = try! CoreDataFeedStore(storeURL: storeURL, bundle: bundle)
         trackForMemoryLeaks(for: sut, file: file, line: line)
         return sut
     }
