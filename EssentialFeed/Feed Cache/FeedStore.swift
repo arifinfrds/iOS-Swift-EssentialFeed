ae322b5c8011c363be966e9b6e55d626d81b90dd
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2021-04-29 14:19:48 +0700

    transform struct `CachedFeed` into tuple to simplify the data definition

--- EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -8,15 +8,7 @@
 
 import Foundation
 
-public struct CachedFeed {
-    public let feed: [LocalFeedImage]
-    public let timestamp: Date
-    
-    public init(feed: [LocalFeedImage], timestamp: Date) {
-        self.feed = feed
-        self.timestamp = timestamp
-    }
-}
+public typealias CachedFeed = (feed: [LocalFeedImage], timestamp: Date)
 
 public protocol FeedStore {
     typealias DeletionCompletion = (Error?) -> Void
