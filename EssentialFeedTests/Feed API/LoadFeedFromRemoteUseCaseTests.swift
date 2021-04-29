71b3e4dfe1acbb12e6451f1b97d016570c42ccfd
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2021-04-29 13:39:33 +0700

    simplify reduce using compact map values

--- EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
+++ EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
@@ -158,11 +158,8 @@ class LoadFeedFromRemoteUseCaseTests: XCTestCase {
             "location": location,
             "image": imageURL.absoluteString
         ]
-        .reduce(into: [String: Any]()) { (acc, e) in
-            if let value = e.value {
-                acc[e.key] = value
-            }
-        }
+        .compactMapValues { $0 }
+        
         return (item, json)
     }
     
