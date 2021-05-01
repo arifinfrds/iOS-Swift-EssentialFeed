a41d2947620f0d66f4f956ce1d1414237820b227
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2021-05-01 12:59:53 +0700

    add fade in logic to mock behavior of async image loading

--- Prototype/Prototype/FeedImageCell.swift
+++ Prototype/Prototype/FeedImageCell.swift
@@ -8,20 +8,48 @@
 import UIKit
 
 final class FeedImageCell: UITableViewCell {
-     @IBOutlet private(set) var locationContainer: UIView!
-     @IBOutlet private(set) var locationLabel: UILabel!
-     @IBOutlet private(set) var feedImageView: UIImageView!
-     @IBOutlet private(set) var descriptionLabel: UILabel!
- }
+    @IBOutlet private(set) var locationContainer: UIView!
+    @IBOutlet private(set) var locationLabel: UILabel!
+    @IBOutlet private(set) var feedImageView: UIImageView!
+    @IBOutlet private(set) var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        feedImageView.alpha = 0
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        feedImageView.alpha = 0
+    }
+    
+    func fadeIn(_ image: UIImage?) {
+        feedImageView.image = image
+        
+        UIView.animate(
+            withDuration: 0.3,
+            delay: 0.3,
+            options: [],
+            animations: {
+                self.feedImageView.alpha = 1
+            })
+    }
+}
 
 extension FeedImageCell {
-     func configure(with model: FeedImageViewModel) {
-         locationLabel.text = model.location
-         locationContainer.isHidden = model.location == nil
-
-         descriptionLabel.text = model.description
-         descriptionLabel.isHidden = model.description == nil
-
-         feedImageView.image = UIImage(named: model.imageName)
-     }
- }
+    func configure(with model: FeedImageViewModel) {
+        locationLabel.text = model.location
+        locationContainer.isHidden = model.location == nil
+        
+        descriptionLabel.text = model.description
+        descriptionLabel.isHidden = model.description == nil
+        
+        if let compressedImageData = UIImage(named: model.imageName)?.jpegData(compressionQuality: 0.9), let compressedImage = UIImage(data: compressedImageData) {
+            fadeIn(compressedImage)
+        } else {
+            feedImageView.image = UIImage(named: model.imageName)
+        }
+    }
+}
