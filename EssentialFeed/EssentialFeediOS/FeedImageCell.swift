662a6a8b113c8e697bac88be10a9b4513ffe3a15
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2021-05-17 11:55:52 +0700

    Render loaded feed on successful load completion

---
+++ EssentialFeed/EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Arifin Firdaus on 17/05/21.
+//  Copyright © 2021 arifinfrds. All rights reserved.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+     public let locationContainer = UIView()
+     public let locationLabel = UILabel()
+     public let descriptionLabel = UILabel()
+ }
