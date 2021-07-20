87580d57eda29cdbe67b59fd8cfc4b18fd441a61
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2021-07-20 18:52:04 +0700

    Extract dependency creation and composition logic from `FeedViewController` into the new `FeedUIComposer`

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -0,0 +1,29 @@
+//
+//  FeedUIComposer.swift
+//  EssentialFeediOS
+//
+//  Created by Arifin Firdaus on 20/07/21.
+//  Copyright © 2021 arifinfrds. All rights reserved.
+//
+
+import Foundation
+import EssentialFeed
+
+public final class FeedUIComposer {
+    
+    public static func feedComposeWith(
+        feedLoader: FeedLoader,
+        imageLoader: FeedImageDataLoader
+    ) -> FeedViewController {
+        
+        let refreshController = FeedRefreshViewController(feedLoader: feedLoader)
+        let feedController = FeedViewController(refreshController: refreshController)
+        
+        refreshController.onRefresh = { [weak feedController] feed in
+            feedController?.tableModel = feed
+                .map { FeedImageCellController(model: $0, imageLoader: imageLoader) }
+        }
+        
+        return feedController
+    }
+}
