129231c35a01a010f757a99a65487d6fc63e0d43
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2021-09-02 11:04:40 +0700

    Move `FeedLoadingViewModel` to separate file

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Arifin Firdaus on 02/09/21.
+//  Copyright © 2021 arifinfrds. All rights reserved.
+//
+
+import Foundation
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
+
