2a434372f17e6b57736b58948cdc6414afd1f4cb
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2021-09-02 11:05:27 +0700

    Move `FeedErrorViewModel` to separate file

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeed
+//
+//  Created by Arifin Firdaus on 02/09/21.
+//  Copyright © 2021 arifinfrds. All rights reserved.
+//
+
+import Foundation
+
+public struct FeedErrorViewModel {
+    public let message: String?
+    
+    static var noError: FeedErrorViewModel {
+        return FeedErrorViewModel(message: nil)
+    }
+    
+    static func error(message: String) -> FeedErrorViewModel {
+        return FeedErrorViewModel(message: message)
+    }
+}
