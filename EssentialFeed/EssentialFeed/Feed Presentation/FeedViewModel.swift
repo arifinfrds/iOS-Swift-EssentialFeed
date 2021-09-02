b28800e282378607c455a375fd80250bf8b02aff
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2021-09-02 11:03:36 +0700

    Move `FeedViewModel` to separate file

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Arifin Firdaus on 02/09/21.
+//  Copyright © 2021 arifinfrds. All rights reserved.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
