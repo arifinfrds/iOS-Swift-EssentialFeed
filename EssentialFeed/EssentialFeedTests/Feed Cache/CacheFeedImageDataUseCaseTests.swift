16c2fc773de446729b148229af399a74047e479b
Arifin Firdaus <arifin.firdaus@icehousecorp.com>
2021-09-29 11:01:59 +0700

    fix indentation

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedImageDataUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedImageDataUseCaseTests.swift
@@ -10,24 +10,24 @@ import XCTest
 import EssentialFeed
 
 class CacheFeedImageDataUseCaseTests: XCTestCase {
-	
+
 	func test_init_doesNotMessageStoreUponCreation() {
 		let (_, store) = makeSUT()
-		
+
 		XCTAssertTrue(store.receivedMessages.isEmpty)
 	}
-	
+
 	func test_saveImageDataForURL_requestsImageDataInsertionForURL() {
 		let (sut, store) = makeSUT()
 		let url = anyURL()
 		let data = anyData()
-		
+
 		sut.save(data, for: url) { _ in }
-		
+
 		XCTAssertEqual(store.receivedMessages, [.insert(data: data, for: url)])
 	}
 	// MARK: - Helpers
-	
+
 	private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedImageDataLoader, store: FeedImageDataStoreSpy) {
 		let store = FeedImageDataStoreSpy()
 		let sut = LocalFeedImageDataLoader(store: store)
@@ -35,5 +35,5 @@ class CacheFeedImageDataUseCaseTests: XCTestCase {
 		trackForMemoryLeaks(for: sut, file: file, line: line)
 		return (sut, store)
 	}
-	
+
 }
