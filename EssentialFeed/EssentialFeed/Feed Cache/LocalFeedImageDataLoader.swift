fea09ffa779b0f96d3699bb223923f0b797e3ebc
Arifin Firdaus <arifin.firdaus@icehousecorp.com>
2021-09-29 10:43:26 +0700

    `LocalFeedImageDataLoader.saveImageDataForURL` requests image data insertion for url into the store

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedImageDataLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedImageDataLoader.swift
@@ -56,4 +56,10 @@ public final class LocalFeedImageDataLoader {
 		}
 		return task
 	}
+
+	public typealias SaveResult = Result<Void, Swift.Error>
+
+	public func save(_ data: Data, for url: URL, completion: @escaping (SaveResult) -> Void) {
+		store.insert(data, for: url) { _ in }
+	}
 }
