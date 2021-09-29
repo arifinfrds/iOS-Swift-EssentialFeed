dbcdfa9940d3455295ce91c09aac21cfceff5f2b
Arifin Firdaus <arifin.firdaus@icehousecorp.com>
2021-09-29 10:53:51 +0700

    Separate retrieval from insertion operations and types to clarify intent

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedImageDataLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedImageDataLoader.swift
@@ -9,57 +9,63 @@
 import Foundation
 
 public final class LocalFeedImageDataLoader {
+	
+	private let store: FeedImageDataStore
+	
+	public init(store: FeedImageDataStore) {
+		self.store = store
+	}
+}
+
+extension LocalFeedImageDataLoader {
+	public typealias SaveResult = Result<Void, LoadError>
+	
+	public func save(_ data: Data, for url: URL, completion: @escaping (SaveResult) -> Void) {
+		store.insert(data, for: url) { _ in }
+	}
+}
 
 
-	private final class Task: FeedImageDataLoaderTask {
-
+extension LocalFeedImageDataLoader: FeedImageDataLoader {
+	public typealias LoadResult = FeedImageDataLoader.Result
+	
+	public enum LoadError: Swift.Error {
+		case failed
+		case notFound
+	}
+	
+	private final class LoadImageDataTask: FeedImageDataLoaderTask {
+		
 		private var completion: ((FeedImageDataLoader.Result) -> Void)?
-
+		
 		init(_ completion: @escaping (FeedImageDataLoader.Result) -> Void) {
 			self.completion = completion
 		}
-
+		
 		func complete(with result: FeedImageDataLoader.Result) {
 			completion?(result)
 		}
-
+		
 		func cancel() {
 			preventFurtherCompletions()
 		}
-
+		
 		private func preventFurtherCompletions() {
 			completion = nil
 		}
 	}
-
-	public enum Error: Swift.Error {
-		case failed
-		case notFound
-	}
-
-	private let store: FeedImageDataStore
-
-	public init(store: FeedImageDataStore) {
-		self.store = store
-	}
-
-	public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-		let task = Task(completion)
+	
+	public func loadImageData(from url: URL, completion: @escaping (LoadResult) -> Void) -> FeedImageDataLoaderTask {
+		let task = LoadImageDataTask(completion)
 		store.retrieve(dataForURL: url) { [weak self] result in
 			guard self != nil else { return }
-
+			
 			task.complete(with: result
-							.mapError { _ in Error.failed }
+							.mapError { _ in LoadError.failed }
 							.flatMap { data in
-				data.map { .success($0) } ?? .failure(Error.notFound)
+				data.map { .success($0) } ?? .failure(LoadError.notFound)
 			})
 		}
 		return task
-	}
-
-	public typealias SaveResult = Result<Void, Swift.Error>
-
-	public func save(_ data: Data, for url: URL, completion: @escaping (SaveResult) -> Void) {
-		store.insert(data, for: url) { _ in }
 	}
 }
