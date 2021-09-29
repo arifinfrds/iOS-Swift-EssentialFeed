f7056f2cb4765afea06c677d1ec03c6a4adf459d
Arifin Firdaus <arifin.firdaus@icehousecorp.com>
2021-09-29 10:09:20 +0700

    `LocalFeedImageDataLoader.loadImageData` requests stored data for URL from `FeedImageDataStore`

--- EssentialFeed/EssentialFeedTests/Feed Cache/LocalFeedImageDataLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/LocalFeedImageDataLoaderTests.swift
@@ -9,9 +9,25 @@
 import XCTest
 import EssentialFeed
 
+protocol FeedImageDataStore {
+	func retrieve(dataForURL url: URL)
+}
+
 final class LocalFeedImageDataLoader {
-	init(store: Any) {
-		
+	
+	private struct Task: FeedImageDataLoaderTask {
+		func cancel() {}
+	}
+	
+	private let store: FeedImageDataStore
+	
+	init(store: FeedImageDataStore) {
+		self.store = store
+	}
+	
+	func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+		store.retrieve(dataForURL: url)
+		return Task()
 	}
 }
 
@@ -23,18 +39,35 @@ class LocalFeedImageDataLoaderTests: XCTestCase {
 		XCTAssertTrue(store.receivedMessages.isEmpty)
 	}
 	
+	func test_loadImageDataFromURL_requestsStoredDataForURL() {
+		let (sut, store) = makeSUT()
+		let url = anyURL()
+		
+		_ = sut.loadImageData(from: url) { _ in }
+		
+		XCTAssertEqual(store.receivedMessages, [.retrieve(dataFor: url)])
+	}
+	
 	// MARK: - Helpers
 	
-	private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedImageDataLoader, store: FeedStoreSpy) {
-		let store = FeedStoreSpy()
+	private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedImageDataLoader, store: StoreSpy) {
+		let store = StoreSpy()
 		let sut = LocalFeedImageDataLoader(store: store)
 		trackForMemoryLeaks(for: store, file: file, line: line)
 		trackForMemoryLeaks(for: sut, file: file, line: line)
 		return (sut, store)
 	}
 	
-	private class FeedStoreSpy {
-		let receivedMessages = [Any]()
+	private class StoreSpy: FeedImageDataStore {
+		enum Message: Equatable {
+			case retrieve(dataFor: URL)
+		}
+		
+		private(set) var receivedMessages = [Message]()
+		
+		func retrieve(dataForURL url: URL) {
+			receivedMessages.append(.retrieve(dataFor: url))
+		}
 	}
 	
 }
