59b9f17094c17e1e57d1770c9284b74a3f8e38a1
Arifin Firdaus <arifin.firdaus@icehousecorp.com>
2021-10-03 19:49:27 +0700

    Fix indentations

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -12,22 +12,22 @@ import CoreData
 public final class CoreDataFeedStore {
 	private let container: NSPersistentContainer
 	private let context: NSManagedObjectContext
-	
+
 	public init(storeURL: URL) throws {
 		let bundle = Bundle(for: CoreDataFeedStore.self)
 		container = try NSPersistentContainer.load(modelName: "FeedStore", url: storeURL, in: bundle)
 		context = container.newBackgroundContext()
 	}
-	
+
 	func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
 		let context = self.context
 		context.perform { action(context) }
 	}
-	
+
 	deinit {
 		cleanUpReferencesToPersistentStores()
 	}
-	
+
 	private func cleanUpReferencesToPersistentStores() {
 		context.performAndWait {
 			let coordinator = self.container.persistentStoreCoordinator
