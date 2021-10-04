173dca7129111b24fd1603a0d744f13388811612
Arifin Firdaus <arifin.firdaus@icehousecorp.com>
2021-10-04 10:32:16 +0700

    `LocalFeedLoader` in integration with the `CoreDataFeedStore` does not delete recently saved items saved by separate instances when validating cache, proving we correctly manage the data models on disk.

--- EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -54,6 +54,19 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
 		expect(feedLoaderToPerformLoad, toLoad: latestFeed)
     }
 
+	func test_validateFeedCache_doesNotDeleteRecentlySavedFeed() {
+		let feedLoaderToPerformSave = makeFeedLoader()
+		let feedLoaderToPerformValidation = makeFeedLoader()
+		let feed = uniqueImageFeed().models
+
+		save(feed, with: feedLoaderToPerformSave)
+		validateCache(with: feedLoaderToPerformValidation)
+
+		expect(feedLoaderToPerformSave, toLoad: feed)
+	}
+
+	// MARK: - LocalFeedImageDataLoader Tests
+
 	func test_loadImageData_deliversSavedDataOnASeparateInstance() {
 		let imageLoaderToPerformSave = makeImageLoader()
 		let imageLoaderToPerformLoad = makeImageLoader()
@@ -66,7 +79,7 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
 
 		expect(imageLoaderToPerformLoad, toLoad: dataToSave, for: image.url)
 	}
-	
+
 	func test_saveImageData_overridesSavedImageDataOnASeparateInstance() {
 		let imageLoaderToPerformFirstSave = makeImageLoader()
 		let imageLoaderToPerformLastSave = makeImageLoader()
@@ -156,6 +169,17 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
 			exp.fulfill()
 		}
 		wait(for: [exp], timeout: 1.0)
+	}
+
+	private func validateCache(with loader: LocalFeedLoader, file: StaticString = #file, line: UInt = #line) {
+		let saveExp = expectation(description: "Wait for save completion")
+		loader.validateCache() { result in
+			if case let Result.failure(error) = result {
+				XCTFail("Expected to validate feed successfully, got error: \(error)", file: file, line: line)
+			}
+			saveExp.fulfill()
+		}
+		wait(for: [saveExp], timeout: 1.0)
 	}
     
     private func setupEmptyStoreState() {
