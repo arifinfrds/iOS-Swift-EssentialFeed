214f2b8207a58312bcfaef6f1a273d0aa26483df
Arifin Firdaus <arifin.firdaus@icehousecorp.com>
2021-10-07 09:01:06 +0700

    Added initial EssentialApp project and EssentialApp workspace (combining the EssentialFeed and EssentialApp projects).

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Arifin Firdaus on 07/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
