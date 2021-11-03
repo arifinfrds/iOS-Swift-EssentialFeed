950a6392fb27c38999c09096de4db350563ebe6c
Arifin Firdaus <arifinfrds.engineer@gmail.com>
2021-11-03 13:06:24 +0700

    Remove unnecessary Main storyboard - the initial set up happens in code

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -35,8 +35,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
         
+        window = UIWindow(windowScene: scene)
         configureWindow()
     }
     
@@ -64,6 +65,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                 )
             )
         )
+        
+        window?.makeKeyAndVisible()
         
     }
     
--- EssentialApp/EssentialAppTests/SceneDelegateTests.swift
+++ EssentialApp/EssentialAppTests/SceneDelegateTests.swift
@@ -11,6 +11,16 @@ import EssentialFeediOS
 
 class SceneDelegateTests: XCTestCase {
     
+    func test_configureWindow_setsWindowAsKeyAndVisible() {
+        let window = UIWindowSpy()
+        let sut = SceneDelegate()
+        sut.window = window
+        
+        sut.configureWindow()
+        
+        XCTAssertEqual(window.makeKeyAndVisibleCount, 1)
+    }
+    
     func test_sceneWillConnectToSession_configuresRootViewController() {
         let sut = SceneDelegate()
         sut.window = UIWindow()
@@ -23,5 +33,15 @@ class SceneDelegateTests: XCTestCase {
         
         XCTAssertNotNil(rootNavigation, "Expected a navigation controller as root, got \(String(describing: root)) instead")
         XCTAssertTrue(topController is FeedViewController, "Expected a feed controller as top view controller, got \(String(describing: topController)) instead")
+    }
+    
+    // MARK: - Helpers
+    
+    private class UIWindowSpy: UIWindow {
+        private(set) var makeKeyAndVisibleCount = 0
+        
+        override func makeKeyAndVisible() {
+            makeKeyAndVisibleCount += 1
+        }
     }
 }
