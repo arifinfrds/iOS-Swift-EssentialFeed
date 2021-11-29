d30e87fde5499138dd1c774e2fea33cef7e5a10b
Arifin Firdaus <arifinfrds.engineer@gmail.com>
2021-11-29 14:45:19 +0700

    Replace FeedLoader composition with Combine operators

--- EssentialApp/EssentialApp/FeedUIComposer.swift
+++ EssentialApp/EssentialApp/FeedUIComposer.swift
@@ -10,44 +10,38 @@ import UIKit
 import EssentialFeed
 import EssentialFeediOS
 
+import UIKit
+import Combine
+import EssentialFeed
+import EssentialFeediOS
+
 public final class FeedUIComposer {
+    private init() {}
     
-    public static func feedComposeWith(
-        feedLoader: FeedLoader,
-        imageLoader: FeedImageDataLoader
-    ) -> FeedViewController {
-        
+    public static func feedComposedWith(feedLoader: @escaping () -> FeedLoader.Publisher, imageLoader: FeedImageDataLoader) -> FeedViewController {
         let presentationAdapter = FeedLoaderPresentationAdapter(
-            feedLoader: MainQueueDispatchDecorator(decoratee: feedLoader)
-        )
+            feedLoader: { feedLoader().dispatchOnMainQueue() })
         
         let feedController = makeFeedViewController(
             delegate: presentationAdapter,
-            title: FeedPresenter.title
-        )
+            title: FeedPresenter.title)
         
         presentationAdapter.presenter = FeedPresenter(
             feedView: FeedViewAdapter(
                 controller: feedController,
-                imageLoader: MainQueueDispatchDecorator(decoratee: imageLoader)
-            ),
+                imageLoader: MainQueueDispatchDecorator(decoratee: imageLoader)),
             loadingView: WeakRefVirtualProxy(feedController),
-            errorView: WeakRefVirtualProxy(feedController)
-        )
+            errorView: WeakRefVirtualProxy(feedController))
         
         return feedController
     }
     
     private static func makeFeedViewController(delegate: FeedViewControllerDelegate, title: String) -> FeedViewController {
-        
         let bundle = Bundle(for: FeedViewController.self)
         let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
         let feedController = storyboard.instantiateInitialViewController() as! FeedViewController
-        
         feedController.delegate = delegate
-        feedController.title = FeedPresenter.title
-        
+        feedController.title = title
         return feedController
     }
 }
-
