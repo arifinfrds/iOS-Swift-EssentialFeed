134fae286133ebdecf98ae5b222e8919c1dbbfec
Arifin Firdaus <arifinfrds.engineer@gmail.com>
2021-12-08 10:58:54 +0700

    Implement generic RemoteLoader

--- EssentialFeed/EssentialFeed/Shared API/RemoteLoader.swift
+++ EssentialFeed/EssentialFeed/Shared API/RemoteLoader.swift
@@ -8,42 +8,44 @@
 
 import Foundation
 
-public final class RemoteLoader: FeedLoader {
-     private let url: URL
-     private let client: HTTPClient
-
-     public enum Error: Swift.Error {
-         case connectivity
-         case invalidData
-     }
-
-     public typealias Result = FeedLoader.Result
-
-     public init(url: URL, client: HTTPClient) {
-         self.url = url
-         self.client = client
-     }
-
-     public func load(completion: @escaping (Result) -> Void) {
-         client.get(from: url) { [weak self] result in
-             guard self != nil else { return }
-
-             switch result {
-             case let .success((data, response)):
-                 completion(RemoteLoader.map(data, from: response))
-
-             case .failure:
-                 completion(.failure(Error.connectivity))
-             }
-         }
-     }
-
-     private static func map(_ data: Data, from response: HTTPURLResponse) -> Result {
-         do {
-             let items = try FeedItemsMapper.map(data, response)
-             return .success(items)
-         } catch {
-             return .failure(Error.invalidData)
-         }
-     }
- }
+public final class RemoteLoader<Resource> {
+    private let url: URL
+    private let client: HTTPClient
+    private let mapper: Mapper
+    
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+    
+    public typealias Result = Swift.Result<Resource, Error>
+    public typealias Mapper = (Data, HTTPURLResponse) throws -> Resource
+    
+    public init(url: URL, client: HTTPClient, mapper: @escaping Mapper) {
+        self.url = url
+        self.client = client
+        self.mapper = mapper
+    }
+    
+    public func load(completion: @escaping (Result) -> Void) {
+        client.get(from: url) { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case let .success((data, response)):
+                completion(self.map(data, from: response))
+                
+            case .failure:
+                completion(.failure(Error.connectivity))
+            }
+        }
+    }
+    
+    private func map(_ data: Data, from response: HTTPURLResponse) -> Result {
+        do {
+            return .success(try mapper(data, response))
+        } catch {
+            return .failure(Error.invalidData)
+        }
+    }
+}
