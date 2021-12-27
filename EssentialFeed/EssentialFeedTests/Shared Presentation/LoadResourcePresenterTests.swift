6db420d04dfcc7a04c1d9c79f48ee222f94c2a33
Arifin Firdaus <arifinfrds.engineer@gmail.com>
2021-12-27 11:28:24 +0700

    Remove unused FeedPresenter logic

--- EssentialFeed/EssentialFeedTests/Shared Presentation/LoadResourcePresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Shared Presentation/LoadResourcePresenterTests.swift
@@ -44,7 +44,7 @@ class LoadResourcePresenterTests: XCTestCase {
     func test_didFinishLoadingWithError_displaysLocalizedErrorMessageAndStopsLoading() {
         let (sut, view) = makeSUT()
         
-        sut.didFinishLoadingFeed(with: anyNSError())
+        sut.didFinishLoading(with: anyNSError())
         
         XCTAssertEqual(view.messages, [
             .display(errorMessage: localized("GENERIC_CONNECTION_ERROR")),
