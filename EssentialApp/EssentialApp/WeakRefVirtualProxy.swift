5c5d9f10a4f0d5064f72fb8a348c0c656d03da65
Arifin Firdaus <arifinfrds.engineer@gmail.com>
2021-12-27 11:54:59 +0700

    Replace FeedImagePresenter with LoadResourcePresenter

--- EssentialApp/EssentialApp/WeakRefVirtualProxy.swift
+++ EssentialApp/EssentialApp/WeakRefVirtualProxy.swift
@@ -10,11 +10,11 @@ import UIKit
 import EssentialFeed
 
 final class WeakRefVirtualProxy<T: AnyObject> {
-    private weak var object: T?
-    
-    init(_ object: T) {
-        self.object = object
-    }
+	private weak var object: T?
+	
+	init(_ object: T) {
+		self.object = object
+	}
 }
 
 extension WeakRefVirtualProxy: ResourceErrorView where T: ResourceErrorView {
@@ -24,13 +24,13 @@ extension WeakRefVirtualProxy: ResourceErrorView where T: ResourceErrorView {
 }
 
 extension WeakRefVirtualProxy: ResourceLoadingView where T: ResourceLoadingView {
-    func display(_ viewModel: ResourceLoadingViewModel) {
-        object?.display(viewModel)
-    }
+	func display(_ viewModel: ResourceLoadingViewModel) {
+		object?.display(viewModel)
+	}
 }
 
-extension WeakRefVirtualProxy: FeedImageView where T: FeedImageView, T.Image == UIImage {
-    func display(_ model: FeedImageViewModel<UIImage>) {
-        object?.display(model)
-    }
+extension WeakRefVirtualProxy: ResourceView where T: ResourceView, T.ResourceViewModel == UIImage {
+	func display(_ model: UIImage) {
+		object?.display(model)
+	}
 }
