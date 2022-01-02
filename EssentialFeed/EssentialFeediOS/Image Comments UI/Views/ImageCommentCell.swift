927b3f143a1b07e24d3a667a01d93fc4667ae2aa
Arifin Firdaus <arifinfrds.engineer@gmail.com>
2022-01-02 16:49:37 +0700

    Implement Image Comments UI

---
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/Views/ImageCommentCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImageCommentCell.swift
+//  EssentialFeediOS
+//
+//  Created by Arifin Firdaus (Personal) on 02/01/22.
+//  Copyright © 2022 arifinfrds. All rights reserved.
+//
+
+import UIKit
+
+public final class ImageCommentCell: UITableViewCell {
+    @IBOutlet private(set) public var messageLabel: UILabel!
+    @IBOutlet private(set) public var usernameLabel: UILabel!
+    @IBOutlet private(set) public var dateLabel: UILabel!
+}
